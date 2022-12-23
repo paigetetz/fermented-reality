5c82eb0ea35e92f1ff6e2c86d65184a66989ab88
Ceri Dorsey-Tyler <cerimay@gmail.com>
2022-12-22 20:56:22 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,52 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroying database..."
+
+Brewery.destroy_all
+User.destroy_all
+
+puts "Database destroyed"
+
+puts "Creating breweries...🍻"
+
+Brewery.create(name:"Brick & Feather", location: "Tuners Falls", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Pioneer Valley", location: "Tuners Falls", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Four Phantoms", location: "Greenfield", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"People's Pint", location: "Greenfield", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Element Brew", location: "Miller's Falls", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Hitchcock Brew", location: "Bernardston", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Brewery @ 4 Star", location: "Northfield", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Back Room Brew", location: "Northfield", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Floodwater Brew", location: "Shelburne Falls", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Stoneman Brew", location: "Colrain", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Treehouse Brew", location: "Deerfield", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Birkshire Brew", location: "S. Deerfield", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Powder Hollow", location: "S. Deerfield", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Brewmasters", location: "Williamsburg", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Amherst Brew", location: "Amherst", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Northhampton Brew", location: "Northhampton", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Building 8 Brew", location: "Northhampton", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Progression Brew", location: "Northhampton", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Fort Hill Brew", location: "Easthampton", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"New City Brew", location: "Easthampton", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Abandoned Building Brew", location: "Easthampton", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Drunkin Rabbit Brew", location: "S. Hadley", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Holyoke Craft Beer", location: "Holyoke", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Leadfoot Brew", location: "Holyoke", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Vanished Valley Brew", location: "Ludlow", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Iron Duke Brew", location: "Ludlow", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Rustic Brewing Co.", location: "Indian Orchard", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Fieldcrest Brew", location: "Wilbraham", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"White Lion Brew", location: "Springfield", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Two Weeks Notice Brew", location: "W. Springfield", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Tin Bridge Brew Co.", location: "Westfield", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Great Awakening Brew", location: "Westfield", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Skyline Beer Co.", location: "Westfield", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Kismet Brew", location: "Westfield", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Brew Practicioners", location: "Longmeadow", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Bright Ideas Brew", location: "North Adams", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Shire Breu-Hous", location: "Dalton", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Wandering Star Craft Brew", location: "Pittsfield", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Barrington Brew", location: "Barrington", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+Brewery.create(name:"Big Elm Brew", location: "Sheffield", url:"", strenth:"", charlies_choice:"", rating:"", amenities:"", image_url:"", vibe:"")
+
+
+puts "Breweries seeded!"
