365ebdc0ea9d43a7555c7439aea66d055aa1fd2c
Ceri Dorsey-Tyler <cerimay@gmail.com>
2022-12-27 11:26:55 -0500

    seeds done

--- client/src/components/BreweryCard.js
+++ client/src/components/BreweryCard.js
@@ -7,7 +7,7 @@ function BreweryCard({brewery}) {
                 <img classname = 'BrewCardImg' src={brewery.image_url} alt={brewery.name} />
                 <h3>{brewery.name}</h3>
                 <p>{brewery.vibe}</p>
-                <h4>{brewery.charlies_choice}</h4>
+                <h4>Charlie's Choice: {brewery.charlies_choice}</h4>
                 <h5>{brewery.strength}</h5>
                 <h5>{brewery.location}</h5>
                 <h6>{brewery.url}</h6>
